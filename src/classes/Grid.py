2b9343ecbcdd77c74f756f2b68b496ec086f9a94
Yinnan Wu <Yinnan.Wu@tum.de>
2021-04-25 22:04:13 +0200

    add comments continuing

--- src/classes/Grid.py
+++ src/classes/Grid.py
@@ -5,7 +5,6 @@ from tkinter import messagebox
 from tkinter import filedialog
 
 import json as js
-import random
 
 from classes.Cell import *
 from classes.Util import *
@@ -37,12 +36,6 @@ class GridWindow:
 
        height : int
            The height of tkinter canvas
-
-
-
-       Attributes
-       ----------
-
 
     """
 
@@ -103,7 +96,13 @@ class GridWindow:
         self.label.pack(side=TOP, padx=2, pady=2)
 
     def draw_grid(self):
+        """
+        draw the grids in the canvas
 
+        Returns
+        -------
+        None
+        """
         for column in range(self.cols):
             for row in range(self.rows):
                 x1 = column * self.cell_width + 4
@@ -114,7 +113,12 @@ class GridWindow:
                 self.cells[row, column] = Cell(row, column)
 
     def clear_grid(self):
-
+        """
+        clear the grids and reset all values
+        Returns
+        -------
+        None
+        """
         self.myCanvas.delete("all")
         self.peds = []
         self.reach_goal = 0
@@ -126,7 +130,13 @@ class GridWindow:
         self.label_text.set(' step')
 
     def init_setup(self):
+        """initial steps when click the load button
 
+        Returns
+        -------
+        None
+
+        """
         # Open json file and read the file id, setting the cells
         self.b_load.config(state=DISABLED)
 
@@ -168,7 +178,14 @@ class GridWindow:
         self.open_count += 1
 
     def open_read_data(self):
+        """open and read json data
 
+        Returns
+        -------
+        dict:
+            json data
+
+        """
         self.input_file = filedialog.askopenfilename(filetypes=[("Json", '*.json'), ("All files", "*.*")])
         if not self.input_file:
             return
@@ -188,6 +205,18 @@ class GridWindow:
         return data
 
     def load_grid(self, data):
+        """get list of pedestirians and set all cells' states
+
+        Parameters
+        ----------
+        data : dict
+            json data
+
+        Returns
+        -------
+        None
+
+        """
         ped = None
         for row, col in data['pedestrians']:
             if self.diff_speed:
@@ -207,8 +236,13 @@ class GridWindow:
             self.cells[row, col].set_state(Cell.OBSTACLE)
 
     def draw_cells(self):
+        """draw the cells (Pedestrians, Obstacle, Target) on the grid
 
-        # draw the cells (Pedestrians, Obstacle, Target) on the grid
+        Returns
+        -------
+        None
+        """
+
         for column in range(self.rows):
             for row in range(self.cols):
                 if self.cells[row, column].get_state() == Cell.PEDESTRIAN:
@@ -227,23 +261,33 @@ class GridWindow:
         self.label_text.set('{} step'.format(self.timestep))
 
     def list_cells(self):
-        # list all cells according to their type
+        """list all cells according to their type
 
-        # self.p_cells = [] # list of origin pedestrian positions [NON-CHANGE]
+        Returns
+        -------
+        None
+
+        """
+
         self.o_cells = []  # list of origin obstacle positions [NON-CHANGE]
         self.t_cells = []  # list of origin target positions [NON-CHANGE]
 
         for column in range(self.rows):
             for row in range(self.cols):
-
-                # if self.cells[row, column].get_state() == 1:
-                # self.p_cells.append(self.cells[row, column])
                 if self.cells[row, column].get_state() == Cell.TARGET:
                     self.t_cells.append(self.cells[row, column])
                 if self.cells[row, column].get_state() == Cell.OBSTACLE:
                     self.o_cells.append(self.cells[row, column])
 
     def handle_conflict(self):
+        """function to handle problem if two pedestrians choose the same next position
+
+        Returns
+        -------
+        list:
+            list of pedestrians chosen to update
+
+        """
         p_to_update = []
         p_to_stay = []
         curr_pos = []
@@ -298,13 +342,18 @@ class GridWindow:
         return p_to_update
 
     def update_cells(self):
-        # update the Pedestrians position per time step
+        """update the Pedestrians position per time step
+
+        Returns
+        -------
+        None
+
+        """
+
         if not self.round_finish:
             self.b_next.config(state=DISABLED)
 
             p_to_update = self.handle_conflict()
-
-            # print(p_to_update)
 
             gen = (p for p in self.peds if p.find_position() in p_to_update)
             for p in gen:
@@ -318,6 +367,12 @@ class GridWindow:
             self.b_next.config(state=NORMAL)
 
     def check_game_end(self):
+        """ check when all pedestrians reach the goal
+
+        Returns
+        -------
+
+        """
         if self.reach_goal == len(self.peds):
             messagebox.showinfo(title='STOP', message='ALL GOAL')
             self.pre_run = True
@@ -328,65 +383,78 @@ class GridWindow:
             self.myFrame.after(self.time, self.update_cells)
 
     def animate(self):
+        """
+        run to update position in canvas automatically
+
+        Returns
+        -------
+        None
+
+        """
         self.animation = True
         self.update_cells()
 
     def get_euclidean_util_map(self):
-        # compute the EuclideanDistance UtilMap
+        """compute the EuclideanDistance UtilMap
+
+        Returns
+        -------
+        None
+
+        """
         self.list_cells()
-        # print(self.t_cells[0].find_position())
 
         self.eu_util_map = EuclideanUtil().compute_util_map(self.rows, self.cols,
                                                             self.t_cells[0],
                                                             self.o_cells)
 
         # plot the EUtilMap as density map
-        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-        ax1 = ax.pcolormesh(self.eu_util_map, vmin=0, vmax=1, cmap='Greens')
-        label_list = np.arange(0, self.rows - 1, 1)
-        label_list = np.append(label_list, self.rows - 1)
-        ax.set_xticks(label_list)
-        ax.set_yticks(label_list)
-        ax.title.set_text('util function')
-        fig.colorbar(ax1, ax=ax)
-        fig.show()
+        # fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        # ax1 = ax.pcolormesh(self.eu_util_map, vmin=0, vmax=1, cmap='Greens')
+        # label_list = np.arange(0, self.rows - 1, 1)
+        # label_list = np.append(label_list, self.rows - 1)
+        # ax.set_xticks(label_list)
+        # ax.set_yticks(label_list)
+        # ax.title.set_text('util function')
+        # fig.colorbar(ax1, ax=ax)
+        # fig.show()
 
     def get_dijkstra_util_map(self):
+        """compute the UtilMap based on dijkstra algorithms
+
+        Returns
+        -------
+        None
+
+        """
         self.list_cells()
         self.dij_util_map = DijkstraUtil().compute_util_map(self.rows, self.cols,
                                                             self.t_cells[0].find_position(),
                                                             self.o_cells)
-        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-        ax1 = ax.pcolormesh(self.dij_util_map, vmin=0, vmax=1, cmap='Greens')
-        label_list = np.arange(0, self.rows - 1, 1)
-        label_list = np.append(label_list, self.rows - 1)
-        ax.set_xticks(label_list)
-        ax.set_yticks(label_list)
-        ax.title.set_text('util function')
-        fig.colorbar(ax1, ax=ax)
-        fig.show()
 
     def get_fmm_util_map(self):
+        """compute the UtilMap based on Fmm algorithms
+
+        Returns
+        -------
+        None
+
+        """
         self.list_cells()
         self.fmm_util_map = FmmUtil().compute_util_map(self.rows, self.cols,
                                                        self.t_cells[0].find_position(),
                                                        self.o_cells)
-        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-        ax1 = ax.pcolormesh(self.fmm_util_map, vmin=0, vmax=1, cmap='Greens')
-        label_list = np.arange(0, self.rows - 1, 1)
-        label_list = np.append(label_list, self.rows - 1)
-        ax.set_xticks(label_list)
-        ax.set_yticks(label_list)
-        ax.title.set_text('util function')
-        fig.colorbar(ax1, ax=ax)
-        fig.show()
 
     def get_interaction_cost_map(self, pedestrian):
+        """compute the interaction map
 
+        Returns
+        -------
+        None
+
+        """
         other_peds = []
         gen = (p for p in self.peds if p.arrived == 0)
         for p in gen:
             if p != pedestrian: other_peds.append(p)
         self.icost_map = InteractionCost().compute_cost_map(self.rows, self.cols, pedestrian, other_peds)
-
-        # print(self.icost_map)
--- src/classes/Strategy.py
+++ src/classes/Strategy.py
@@ -2,7 +2,24 @@ import numpy as np
 
 
 def find_best_neighbor_diag(m, r, c):
-    # return the position of neighbor with smallest util around cell@(r,c) based on UtilMap m
+    """
+    find the best neighbor using Moore Neighborhood definition, which contents the diagonal position
+
+    Parameters
+    ----------
+    m : array shape(rows, cols)
+        utility map
+    r : int
+        row of cell which need to find the best neighbor
+    c : int
+        col of cell which need to find the best neighbor
+
+    Returns
+    -------
+    tuple:
+        the position of neighbor with smallest util around cell@(r,c) based on UtilMap m
+    """
+
     neighbors = []
     best_n = (r, c)
     min_u = np.inf
@@ -19,13 +36,28 @@ def find_best_neighbor_diag(m, r, c):
                     if m[new_r, new_c] <= min_u:
                         best_n = (new_r, new_c)
                         min_u = m[new_r, new_c]
-
-    # print(neighbors)
-    # print(best_n)
     return best_n
 
 
 def find_best_neighbor_v_h(m, r, c):
+    """
+    find the best neighbor using Von Neumann neighborhood definition, which contents only vertically
+    and horizontally position
+
+    Parameters
+    ----------
+    m : array shape(rows, cols)
+        utility map
+    r : int
+        row of cell which need to find the best neighbor
+    c : int
+        col of cell which need to find the best neighbor
+
+    Returns
+    -------
+    tuple:
+        the position of neighbor with smallest util around cell@(r,c) based on UtilMap m
+    """
     best_n = (r, c)
     min_u = np.inf
 
@@ -47,6 +79,23 @@ def find_best_neighbor_v_h(m, r, c):
 
 
 def find_best_neighbor_total(m, r, c):
+    """
+    find the best neighbor in a timestep for higher speed pedestrian, which search in a wide neighbors
+
+    Parameters
+    ----------
+    m : array shape(rows, cols)
+        utility map
+    r : int
+        row of cell which need to find the best neighbor
+    c : int
+        col of cell which need to find the best neighbor
+
+    Returns
+    -------
+    tuple:
+        the position of neighbor with smallest util around cell@(r,c) based on UtilMap m
+    """
     best_n = (r, c)
     min_u = np.inf
 
